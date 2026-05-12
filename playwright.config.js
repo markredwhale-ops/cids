021b1de372e127564534385f241cc26520de2f22
Miralim Alimov <markredwhale@gmail.com>
2026-05-11 20:16:56 -0400

    Jenkinsfile

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  testMatch: '/*.spec.js',  // 
+  testIgnore: '/node_modules/**',  //
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,7 +25,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }], // для детального HTML-отчета
+    ['junit', { outputFile: 'test-results/junit.xml' }] // для обработки Jenkins'ом
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,6 +36,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: true,
   },
 
   /* Configure projects for major browsers */
